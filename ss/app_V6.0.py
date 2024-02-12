e88ab28b426d00b4bb9d06a8a626913b24c78036
Sushmit9 <sushmit9@schulich.yorku.ca>
2024-02-12 18:28:14 -0500

    Speech to text correction

--- ss/app_V6.0.py
+++ ss/app_V6.0.py
@@ -199,6 +199,6 @@ def main():
             with st.chat_message("assistant"):
                 st.session_state.messages.append({"role": "assistant", "content": response})
                 st.write(response)
-                
+
 if __name__ == '__main__':
     main()
